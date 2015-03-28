fffea534d4a117df42630e02920e290252afd535
Feanathiel <Feanathiel@users.noreply.github.com>
2015-03-28 19:27:56 +0100

    Age distribution is pretty constant; removed.

--- CityWebServer/wwwroot/citizen/index.js
+++ CityWebServer/wwwroot/citizen/index.js
@@ -4,7 +4,6 @@ define([
     'citizen/services/Citizen',
     'citizen/controllers/citizen',
     'citizen/controllers/population',
-    'citizen/controllers/ageDistribution',
     'citizen/controllers/birthAndDeath',
     'citizen/controllers/educationEmployment'
 ], function () { });
